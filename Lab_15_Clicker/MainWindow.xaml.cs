c2474cc6651a9c522a48a7f021eb9ad9d6e16587
Ivan <shcmalivan@gmail.com>
2025-11-14 22:26:35 +0300

    Add files via upload

---
+++ Lab_15_Clicker/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace Lab_15_Clicker
+{
+    public partial class MainWindow : Window
+    {
+        private int _counter = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            UpdateCounterText();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            _counter++;
+            UpdateCounterText();
+        }
+        private void UpdateCounterText()
+        {
+            CounterText.Text = $"Счетчик: {_counter}";
+        }
+    }
+}
